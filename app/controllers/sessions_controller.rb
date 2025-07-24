fbf1c077f1f0d96eea1e71ff8c6efbb15b8fcc83
cusnaruto <hoangvip3333@gmail.com>
2025-07-24 14:07:15 +0700

    Chapter 9
    Chapter 9
    
    Chapter 9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,30 +1,49 @@
 class SessionsController < ApplicationController
+  REMEMBER_ME_CONST = "1"
+
   before_action :load_user, only: :show
+  before_action :find_user_by_email, only: :create
   before_action :authenticate_user, only: :create
 
   def show; end
 
-  # POST: /login
+  def new; end
+
   def create
-    reset_session
-    log_in @user
-    redirect_to session_path(@user), status: :see_other
+    handle_successful_authentication(@user)
   end
 
   private
+
+  def find_user_by_email
+    @user = User.find_by(email: params.dig(:session, :email)&.downcase)
+    return if @user
+
+    flash.now[:danger] = t("sessions.create.user_not_found")
+    render :new, status: :unprocessable_entity
+  end
+
+  def authenticate_user
+    return if @user&.authenticate(params.dig(:session, :password))
+
+    handle_failed_authentication
+  end
+
+  def handle_successful_authentication(user)
+    log_in user
+    params.dig(:session, :remember_me) == REMEMBER_ME_CONST ? remember(user) : forget(user)
+    redirect_to session_path(user), status: :see_other
+  end
+
+  def handle_failed_authentication
+    flash.now[:danger] = t("sessions.create.invalid_email_or_password")
+    render :new, status: :unprocessable_entity
+  end
 
   def load_user
     @user = User.find_by(id: params[:id])
     return if @user
 
     redirect_to root_path, alert: t("sessions.show.user_not_found")
-  end
-
-  def authenticate_user
-    @user = User.find_by(email: params.dig(:session, :email)&.downcase)
-    return if @user&.authenticate params.dig(:session, :password)
-
-    flash.now[:danger] = t("sessions.create.invalid_email_or_password")
-    render :new, status: :unprocessable_entity
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   PASSWORD_MAXIMUM_LENGTH = 255
   AGE_MAXIMUM = 100.years
 
+  attr_accessor :remember_token
+
   enum gender: {male: 0, female: 1, other: 2}
 
   has_secure_password
@@ -16,6 +18,35 @@ class User < ApplicationRecord
 
   validate :dob_valid
 
+  def self.digest string
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create string, cost:
+  end
+
+  # Generate a random token
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
   private
 
   def dob_valid
@@ -26,14 +57,5 @@ class User < ApplicationRecord
     elsif dob < AGE_MAXIMUM.ago.to_date
       errors.add(:dob, :too_old)
     end
-  end
-
-  def self.digest string
-    cost = if ActiveModel::SecurePassword.min_cost
-             BCrypt::Engine::MIN_COST
-           else
-             BCrypt::Engine.cost
-           end
-    BCrypt::Password.create string, cost:
   end
 end
